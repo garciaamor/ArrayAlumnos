3ec6d1700c4e139a0b9c878580181b92bf8d2be6
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-01-20 11:06:52 +0100

    Ejercicio Array Alumnos

--- src/arrayalumnos/ArrayAlumnos.java
+++ src/arrayalumnos/ArrayAlumnos.java
@@ -1,12 +1,34 @@
 package arrayalumnos;
 
+import javax.swing.JOptionPane;
+
 public class ArrayAlumnos {
 
     public static void main(String[] args) {
         //Declarar un Array de tamaño 5
         MetodosArrayAlumnos arr=new MetodosArrayAlumnos();
-        arr.cargarArray();
-        arr.visualizar();
+        int res;     
+        do{
+       res= Integer.parseInt(JOptionPane.showInputDialog("Elija la opcion deseada \n 1.Introducir alumnos y notas \n 2.Mostrar las notas \n 3. Consultar las notas \n 4. Ordenar las nota de mayor a menor \n 5. Ordenar las notas de menor a mayor \n 6. Finalizar programa  "));
+        switch (res){
+            case 1:arr.cargarArray();
+                   break;
+            case 2:arr.visualizar();
+                   break;
+            case 3:arr.consultarNota();
+                  break;
+            case 4:arr.maytomen();
+                break;
+            case 5:arr.mentomay();
+                break;
+            //case 6:arr.ordenome();
+              //  break;
+            case 7:System.exit(0);
+        }}
+          while (res<7);
+        
     }
-    
-}
+        
+    }
+
+
--- src/arrayalumnos/MetodosArrayAlumnos.java
+++ src/arrayalumnos/MetodosArrayAlumnos.java
@@ -1,6 +1,7 @@
 package arrayalumnos;
 
 import javax.swing.JOptionPane;
+import libreria.Metodos;
 
 public class MetodosArrayAlumnos {
     
@@ -8,6 +9,8 @@ public class MetodosArrayAlumnos {
         
        String []nomes=new String[numelementos];
        float []notas=new float[numelementos];
+       float aux;
+       String auxn;
        
        public int pedirElementos(){
            return Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de alumnos") );
@@ -25,9 +28,52 @@ public class MetodosArrayAlumnos {
                
            }}
        public void visualizar(){
-        for(int i=0;i<numelementos;i++)
-        System.out.println("El alumno "+ nomes[i]+" tiene un " + notas[i]);
+        //for(int i=0;i<numelementos;i++)
+        //System.out.println("El alumno "+ nomes[i]+" tiene un " + notas[i]);
+        String cadea="";
+        for(int y=0;y<nomes.length;y++){
+            cadea=cadea + nomes[y]+"-->"+notas[y]+"\n";
+            JOptionPane.showMessageDialog(null, cadea);
+        }
        }
+       
+       public void mentomay(){
+           for (int i=0;i<numelementos-1;i++)
+               for (int j=i+1;j<numelementos;j++)
+                   if (notas[i]>notas[j]){
+                       aux=notas[i];
+                       notas[i]=notas[j];
+                       notas[j]=aux;
+                       auxn=nomes[i];
+                       nomes[i]=nomes[j];
+                       nomes[j]=auxn;                       
+                   }
+                  }
+       public void maytomen(){
+           for (int i=0;i<numelementos-1;i++)
+               for (int j=i+1;j<numelementos;j++)
+                   if (notas[i]<notas[j]){
+                       aux=notas[i];
+                       notas[i]=notas[j];
+                       notas[j]=aux;
+                       auxn=nomes[i];
+                       nomes[i]=nomes[j];
+                       nomes[j]=auxn;                       
+                   }
+                  }
+       public void consultarNota(){
+           String nomBus=JOptionPane.showInputDialog("nome a buscar");
+           boolean atopado=false;//non se atopou
+           for (int i=0;i<nomes.length;i++){
+               if ((nomes[i].compareToIgnoreCase(nomBus)==0)){
+                   JOptionPane.showMessageDialog(null, "nota = "+notas[i]);
+                   atopado=true;
+               }
+           }
+       }
+           
+           
+       
 }
 
 
